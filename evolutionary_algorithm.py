444457bffa4e9fcf90abba0eaf50be212a4a3613
TristanJP <trisperk@hotmail.com>
2019-11-07 16:13:27 +0000

    start

---
+++ evolutionary_algorithm.py
@@ -0,0 +1,141 @@
+# Evolutionary Algorithm
+#
+# Experiment Setup
+# -------------------------------
+# Representation: Route = List of integers associated with cities e.g. [0,1,2,3,4,5]
+# Fitness Evaluation: get_cost_of_route() function which adds up the distances between all cities in a route
+# Recombination:
+# Recombination Probability:
+# Mutation:
+# Mutation Probability:
+# Parent Selection:
+# Survivor Selection:
+# Population Size: Test = 10, Actual = ?
+# Initialisation:
+# Termination:
+#
+
+import math
+import random
+import time
+
+def get_cities_from_file(file_name):
+    csv_file = open(file_name, "r")
+    cities_map = []
+    for line in csv_file:
+        city = []
+        for coord in line.split(","):
+            coord = coord.rstrip()
+            city += [float(coord)]
+        cities_map += [city]
+
+    return cities_map
+
+def get_list_of_cities(cities_map):
+    return list(range(len(cities_map)))
+
+def get_cost_of_route(route, cities_map):
+    total = 0
+    i = 0
+
+    while i < len(route)-1:
+        total += get_cost_between_cities(cities_map, route[i],route[i+1])
+        i += 1
+    total += get_cost_between_cities(cities_map, route[-1], route[0])
+
+    return total
+
+def get_cost_between_cities(cities_map, city_1, city_2):
+
+    return distance_between_coords(cities_map[city_1][0],
+                                   cities_map[city_2][0],
+                                   cities_map[city_1][1],
+                                   cities_map[city_2][1])
+
+def distance_between_coords(x1, x2, y1, y2):
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def generate_random_route(city_list):
+    copy = city_list[1:]
+    random.shuffle(copy)
+    city_list[1:] = copy
+    return city_list
+
+def get_neighbourhood_of_route(route):
+    neighbourhood = []
+    city_1 = 0
+    while city_1 < len(route):
+        city_2 = 0
+        while city_2 < len(route):
+            new_route = route.copy()
+            new_route[city_1] = route[city_2]
+            new_route[city_2] = route[city_1]
+            if new_route not in neighbourhood:
+                neighbourhood.append(new_route.copy())
+            city_2 += 1
+        city_1 += 1
+    return neighbourhood
+
+def find_shortest_route_in_neighbourhood(neighbourhood, cities_map):
+    shortest_route = neighbourhood[0]
+    cost = get_cost_of_route(neighbourhood[0], cities_map)
+    for route in neighbourhood:
+        new_cost = get_cost_of_route(route, cities_map)
+        if cost > new_cost:
+            cost = new_cost
+            shortest_route = route
+    return shortest_route
+
+def find_shortest_routes_with_neighbourhood(cities_map, time_limit):
+    cities_list = get_list_of_cities(cities_map)
+    route = generate_random_route(cities_list)
+    shortest_route = route
+
+    start_time = time.time()
+    elapsed_time = 0
+    i = 1
+    while elapsed_time < time_limit:
+        neighbourhood = get_neighbourhood_of_route(route)
+        new_route = find_shortest_route_in_neighbourhood(neighbourhood, cities_map)
+
+        if route == new_route:
+            new_cost = get_cost_of_route(new_route, cities_map)
+            if get_cost_of_route(shortest_route, cities_map) > new_cost:
+                shortest_route = new_route
+                #print(f"New Shortest: {shortest_route}  [{new_cost}]")
+            #else:
+                #print("Local best not short enough.")
+            route = generate_random_route(cities_list)
+            
+        else:
+            route = new_route
+
+
+        #print(f'{i}: {route}    [{get_cost_of_route(route, cities_map)}]')
+        i += 1
+
+        elapsed_time = time.time() - start_time
+    
+    print(f"\n=============== FINISHED ===============\nShortest Size: {get_cost_of_route(shortest_route, cities_map)}\nShortest Routes: {shortest_route}\nChecked {i} solutions")
+
+    return shortest_route
+
+
+
+## ======================================================================
+## Program Run
+start_time = time.time()
+
+
+cities_map = get_cities_from_file("ulysses16(1).csv")
+
+#rand_shortest = find_shortest_routes_randomly(cities_map, 10)
+
+local_search = find_shortest_routes_with_neighbourhood(cities_map, 1)
+
+#print(get_cost_of_route([1,2,3,7,0,6,5,4,8], cities_map))
+
+## Program End
+end_time = time.time()
+## ======================================================================
+print(f"\n\nTime: {end_time-start_time}\n========================================")
